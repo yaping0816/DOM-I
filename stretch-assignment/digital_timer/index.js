7ba9cf0a80d78d752739023c359e3440facdc2df
Yaping Liu <yaping.liu12@outlook.com>
2021-05-03 13:37:08 -0700

    updated the start timers and add a reset timer. but the timer won't start from 0 when clicking the start button after the reset

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,58 +1,40 @@
-// 10 seconds count-up timer
+// // 10 seconds count-up timer
 
-let timer = setInterval(countTimer, 10);
+// let timer = setInterval(countTimer, 10);
 
-let totalMsTens = 0;
-let countStart =  false;
+// let totalMsTens = 0;
 
-function countTimer(){
-    console.log('hii');
-    if(countStart){
+// function countTimer(){
+//    
         
-        if(totalMsTens <= 1000){
-            let secondTens = Math.floor(totalMsTens / 1000);
-            let secondOnes = Math.floor((totalMsTens - secondTens * 1000) / 100);
+//         if(totalMsTens <= 1000){
+//             let secondTens = Math.floor(totalMsTens / 1000);
+//             let secondOnes = Math.floor((totalMsTens - secondTens * 1000) / 100);
             
-            let msHundreds = Math.floor((totalMsTens - secondTens * 1000 - secondOnes * 100) / 10);
-            let msTens = Math.floor(totalMsTens - secondTens * 1000 - secondOnes * 100 - msHundreds * 10);
+//             let msHundreds = Math.floor((totalMsTens - secondTens * 1000 - secondOnes * 100) / 10);
+//             let msTens = Math.floor(totalMsTens - secondTens * 1000 - secondOnes * 100 - msHundreds * 10);
      
-             document.querySelector('#msTens').innerHTML = msTens;
-             document.querySelector('#msHundreds').innerHTML = msHundreds;
-             document.querySelector('#secondOnes').innerHTML = secondOnes;
-             document.querySelector('#secondTens').innerHTML = secondTens;
+//              document.querySelector('#msTens').innerHTML = msTens;
+//              document.querySelector('#msHundreds').innerHTML = msHundreds;
+//              document.querySelector('#secondOnes').innerHTML = secondOnes;
+//              document.querySelector('#secondTens').innerHTML = secondTens;
      
-             totalMsTens ++;
+//              totalMsTens ++;
              
      
-         }else clearInterval(timer);
-    }
+//          }else clearInterval(timer);
+//     
 
     
-    const digit = document.querySelectorAll('.digit');
+//     const digit = document.querySelectorAll('.digit');
 
-    if(totalMsTens === 1000) {
-        digit.forEach( item => item.classList.add('redDigit'));
-    }
+//     if(totalMsTens === 1000) {
+//         digit.forEach( item => item.classList.add('redDigit'));
+//     }
     
-}
-// add button to the timer
-const startButton = document.createElement('button');
-const resetButton = document.createElement('button');
-startButton.textContent = 'start';
-resetButton.textContent = 'reset';
-const body = document.querySelector('body');
-body.appendChild(startButton);
-body.appendChild(resetButton);
+// }
 
-startButton.addEventListener('click', event =>{
-    countStart = true;
-})
-
-
-
-
-
-
+// another solution
 // let msCount10 = 0;
 // const msTens = document.querySelector('#msTens');
 // const msHundreds = document.querySelector('#msHundreds');
@@ -83,3 +65,69 @@ startButton.addEventListener('click', event =>{
 
 // }
 
+// 10 seconds count-up timer with start button and reset button
+let timer;
+function time(cmd) {
+    if(cmd === 'start'){
+        timer = setInterval(countTimer, 10);
+    }
+
+    if(cmd === 'reset'){
+        clearInterval(timer);
+        secondTens.textContent = 0;
+        secondOnes.textContent = 0;
+        msTens.textContent = 0;
+        msHundreds.textContent = 0;
+    }
+}
+
+let totalMsTens = 0;
+let countStart =  false;
+
+function countTimer(){
+    console.log('hii');
+    // if(countStart){
+        
+        if(totalMsTens <= 1000){
+            let secondTens = Math.floor(totalMsTens / 1000);
+            let secondOnes = Math.floor((totalMsTens - secondTens * 1000) / 100);
+            
+            let msHundreds = Math.floor((totalMsTens - secondTens * 1000 - secondOnes * 100) / 10);
+            let msTens = Math.floor(totalMsTens - secondTens * 1000 - secondOnes * 100 - msHundreds * 10);
+     
+             document.querySelector('#msTens').innerHTML = msTens;
+             document.querySelector('#msHundreds').innerHTML = msHundreds;
+             document.querySelector('#secondOnes').innerHTML = secondOnes;
+             document.querySelector('#secondTens').innerHTML = secondTens;
+     
+             totalMsTens ++;
+             
+     
+         }else time('stop');
+    // }
+
+    
+    const digit = document.querySelectorAll('.digit');
+
+    if(totalMsTens === 1000) {
+        digit.forEach( item => item.classList.add('redDigit'));
+    }
+    
+}
+// add button to the timer
+const startButton = document.createElement('button');
+const resetButton = document.createElement('button');
+startButton.textContent = 'start';
+resetButton.textContent = 'reset';
+const body = document.querySelector('body');
+body.appendChild(startButton);
+body.appendChild(resetButton);
+
+startButton.addEventListener('click', event =>{
+    // countStart = true;
+    time('start');
+})
+    
+resetButton.addEventListener('click', event=>{
+    time('reset');
+})
